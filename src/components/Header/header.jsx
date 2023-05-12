7483e9925a8d1a90797af0ec0f73c7a0e685e818
achrafAR <achraf.alrachini@gmail.com>
2023-05-12 09:08:23 +0300

    starting the frontend in the final project

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './header.css'
+import accountIcon from '../../icons/login.png'
+function header() {
+
+    const navbar = [
+        { name: 'HOME', link: '/' },
+        { name: 'OFFERS', link: '/' },
+        { name: 'ABOUT US', link: '/' },
+        { name: 'CONTACT US', link: '/' }
+
+    ]
+
+
+
+
+    return (
+        <div className='header'>
+            <div className='header__logo'>
+            <img className='logo' src={accountIcon} alt ='login'/>
+
+            </div>
+            <div className='header__nav'>
+                {
+                    navbar.map((item, index) => (
+                        <div key={index} className='header__nav__item'>
+                            <a href={item.link}>{item.name}</a>
+                            </div>
+                            ))
+                }
+            </div>
+            <div className='icon_login_explore'>
+                <div className='icon_account'>
+                    <img className='icon_login' src={accountIcon} alt ='login'/>
+                </div>
+                <div className='explore_btn'>
+                    <a href= "#" className='explore_btn_header'>Expore Now</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default header
